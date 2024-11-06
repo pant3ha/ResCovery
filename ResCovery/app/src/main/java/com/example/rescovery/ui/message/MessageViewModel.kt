aae8c4bf1d9bc697c12f164436af9a6e4f6b8b37
Seulah Kim <k.urb39@gmail.com>
2024-11-06 13:20:12 -0800

    created fragments for each

--- ResCovery/app/src/main/java/com/example/rescovery/ui/message/MessageViewModel.kt
+++ ResCovery/app/src/main/java/com/example/rescovery/ui/message/MessageViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.rescovery.ui.dashboard
+package com.example.rescovery.ui.message
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class MessageViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is message Fragment"
     }
     val text: LiveData<String> = _text
 }
